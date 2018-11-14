fb00cc8908a237c1d7ad0bbfd2c5f8e6b454440c
dangerous <gm.davi.gm@live.com>
2018-11-14 11:01:58 -0200

    Alterações na geração do PDF para gerar títulos corretamente em UTF-8.

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jung-kurt/gofpdf"
 	"github.com/wcharczuk/go-chart"
 	"io"
+	"os"
 	"time"
 )
 
@@ -44,16 +45,27 @@ func generatePDF(ids []int, dataInicial []uint64, dataFinal []uint64, dataEntreg
 	var atrasadas = alocacoes.Select(func(index int, value interface{}) bool {
 		aloc, _ := value.(Alocacao)
 		return aloc.dataFinal < aloc.dataEntrega
-	})
+	}).Size()
 
+	var pendentes = alocacoes.Select(func(index int, value interface{}) bool {
+		aloc, _ := value.(Alocacao)
+		return aloc.dataEntrega == 0
+	}).Size()
+
+	var entregues = alocacoes.Select(func(index int, value interface{}) bool {
+		aloc, _ := value.(Alocacao)
+		return aloc.dataEntrega != 0 && aloc.dataFinal >= aloc.dataEntrega
+	}).Size()
 
 	pdf := gofpdf.New("P", "mm", "A4", "")
 
+	tr := pdf.UnicodeTranslatorFromDescriptor("")
+
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, fmt.Sprintf("Relatório mensal das vendas realizadas em %s", time.Now().Format("2006-01-02")))
+	pdf.Cell(40, 10, tr(fmt.Sprintf("Relatório mensal das vendas realizadas em %s", time.Now().Format("2006-01-02"))))
 	pdf.SetAuthor("Netflix", true)
-	pdf.SetSubject("Relatório mensal", true)
+	pdf.SetSubject(tr("Relatório mensal"), true)
 	pdf.SetCreationDate(time.Now())
 
 	sbc := chart.BarChart{
@@ -61,21 +73,20 @@ func generatePDF(ids []int, dataInicial []uint64, dataFinal []uint64, dataEntreg
 		TitleStyle: chart.StyleShow(),
 		Background: chart.Style{
 			Padding: chart.Box{
-				Top: 40,
+				Top: 60,
 			},
 		},
 		Height:   512,
 		BarWidth: 60,
+		Width:    800,
 		XAxis:    chart.StyleShow(),
 		YAxis: chart.YAxis{
 			Style: chart.StyleShow(),
 		},
 		Bars: []chart.Value{
-			{Value: , Label: "Atrasadas"},
-			{Value: 5.25, Label: "America"},
-			{Value: 4.88, Label: "Brasil"},
-			{Value: 4.74, Label: "USA"},
-			{Value: 3.22, Label: "CNA"},
+			{Value: float64(atrasadas), Label: "Atrasadas"},
+			{Value: float64(pendentes), Label: "Pendentes"},
+			{Value: float64(entregues), Label: "Entregues"},
 		},
 	}
 
@@ -86,9 +97,47 @@ func generatePDF(ids []int, dataInicial []uint64, dataFinal []uint64, dataEntreg
 		panic(err2)
 	}
 
-	image := pdf.RegisterImageOptionsReader("grafico", gofpdf.ImageOptions{ImageType: "png", ReadDpi: true}, io.Reader(buffer))
+	image := pdf.RegisterImageOptionsReader("Alocações realizadas", gofpdf.ImageOptions{ImageType: "png", ReadDpi: true}, io.Reader(buffer))
 
-	pdf.Image("grafico", 0, 10, image.Width()/3, image.Height()/3, false, "", 0, "")
+	pdf.Image("Alocações realizadas", 0, 50, image.Width()/3, image.Height()/3, false, "", 0, "")
+
+	var vendas float64
+
+	for _, v := range valores {
+		vendas += v
+	}
+
+	sbc = chart.BarChart{
+		Title:      "Arrecadações",
+		TitleStyle: chart.StyleShow(),
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 60,
+			},
+		},
+		Height:   512,
+		BarWidth: 60,
+		Width:    800,
+		XAxis:    chart.StyleShow(),
+		YAxis: chart.YAxis{
+			Style: chart.StyleShow(),
+		},
+		Bars: []chart.Value{
+			{Value: vendas, Label: "Vendas"},
+			{Value: 0, Label: "Multas"},
+		},
+	}
+
+	buffer = bytes.NewBuffer([]byte{})
+	err2 = sbc.Render(chart.PNG, buffer)
+
+	if err2 != nil {
+		panic(err2)
+	}
+
+	image = pdf.RegisterImageOptionsReader("Arrecadações", gofpdf.ImageOptions{ImageType: "png", ReadDpi: true}, io.Reader(buffer))
+
+	pdf.Image("Arrecadações", 100, 50, image.Width()/3, image.Height()/3, false, "", 0, "")
 
 	pdfBuffer := bytes.NewBuffer([]byte{})
 
@@ -99,6 +148,31 @@ func generatePDF(ids []int, dataInicial []uint64, dataFinal []uint64, dataEntreg
 
 func main() {
 	fmt.Print("hello")
+
+	ids := []int{1, 5, 8, 7}
+
+	dataInicial := []uint64{1, 4, 8, 7}
+
+	dataFinal := []uint64{1, 4, 8, 7}
+
+	dataEntrega := []uint64{1, 4, 8, 7}
+
+	valores := []float64{1, 4, 8, 7}
+
+	bytes, _ := generatePDF(ids, dataInicial, dataFinal, dataEntrega, valores)
+
+	file, err := os.Create("hel.pdf")
+
+	if err != nil {
+		panic(err)
+	}
+
+	file.Write(bytes)
+
+	file.Sync()
+
+	file.Close()
+
 	/*pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
